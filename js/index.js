722dcd1ebaf1e32a4c107f9664c060e5aab8ad9e
Greyson <greyson.r.hamilton@gmail.com>
2020-09-30 21:19:45 -0400

    no more bugs. next task

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ navHead[5].textContent = siteContent.nav["nav-item-6"];
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent.nav["img-src"]);
 
 const hText = document.getElementsByClassName('cta-text')[0].children[0];
 hText.textContent = siteContent.cta.h1;
@@ -56,13 +56,43 @@ const newButton = document.getElementsByClassName('cta-text')[0].children[1];
 newButton.textContent = siteContent.cta.button;
 
 const ctaImg = document.getElementById('cta-img');
-ctaImg.setAttribute('scr', siteContent['cta']['img-src']);
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+const h4Feature = document.querySelector('div.top-content div.text-content:first-child h4');
+h4Feature.textContent = siteContent['main-content']['features-h4'];
+
+const featContent = document.querySelector('div.top-content div.text-content:first-child p')
+featContent.textContent = siteContent['main-content']['features-content'];
+
+const h4About = document.querySelector('div.top-content div.text-content:nth-child(2) h4');
+h4About.textContent = siteContent['main-content']['about-h4'];
+
+const aboutContent = document.querySelector('div.top-content div.text-content:nth-child(2) p')
+aboutContent.textContent = siteContent['main-content']['about-content'];
 
 const middleImg = document.getElementById('middle-img');
 middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
 
+const h4Services = document.querySelector('div.bottom-content div.text-content:first-child h4');
+h4Services.textContent = siteContent['main-content']['services-h4'];
+
+const servicesContent = document.querySelector('div.bottom-content div.text-content:first-child p');
+servicesContent.textContent = siteContent['main-content']['services-content'];
+
+const h4Product = document.querySelector('div.bottom-content div.text-content:nth-child(2) h4');
+h4Product.textContent = siteContent['main-content']['product-h4'];
+
+const prodContent = document.querySelector('div.bottom-content div.text-content:nth-child(2) p')
+prodContent.textContent = siteContent['main-content']['product-content'];
+
+const h4Vision = document.querySelector('div.bottom-content div.text-content:nth-child(3) h4');
+h4Vision.textContent = siteContent['main-content']['vision-h4'];
+
+const visionContent = document.querySelector('div.bottom-content div.text-content:nth-child(3) p')
+visionContent.textContent = siteContent['main-content']['vision-content'];
+
 const addy = document.querySelector('.contact h4');
-contact.textContent = siteContent.contact['contact-h4'];
+addy.textContent = siteContent.contact['contact-h4'];
 
 const addyInfo = document.querySelectorAll('.contact p');
 addyInfo[0].textContent = siteContent.contact.address;
